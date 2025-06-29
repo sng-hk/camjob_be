83d95e962cb105d53d2e0c1d01526377835de59f
sng-hk <csh091456743@gmail.com>
2025-06-29 18:46:27 +0900

    filter 추가

---
+++ src/main/java/com/example/camjob/filter/LoggingFilter.java
@@ -0,0 +1,22 @@
+package com.example.camjob.filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class LoggingFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        System.out.println("[요청 로그] " + req.getMethod() + " " + req.getRequestURI());
+        chain.doFilter(request, response);
+    }
+}
