0135cf64e2737c3817bca6c98c5d38dce169938b
sng-hk <csh091456743@gmail.com>
2025-06-29 16:08:48 +0900

    fix : cors 정책 수정 (개발용)

--- src/main/java/com/example/camjob/config/WebConfig.java
+++ src/main/java/com/example/camjob/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // 모든 엔드포인트에 대해
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(false);
