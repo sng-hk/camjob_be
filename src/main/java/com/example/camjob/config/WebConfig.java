a658a619a4c5e1e6c6d2c6d6f0b2883c5d6362e2
sng-hk <csh091456743@gmail.com>
2025-06-30 02:48:01 +0900

    security 수정

--- src/main/java/com/example/camjob/config/WebConfig.java
+++ src/main/java/com/example/camjob/config/WebConfig.java
@@ -14,10 +14,10 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // 모든 엔드포인트에 대해
-                        .allowedOrigins("https://campjob.netlify.app")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowCredentials(false);
             }
         };
     }
