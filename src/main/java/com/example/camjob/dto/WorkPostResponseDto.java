fc199bb06cd00625eb374e0a79b8b32e4ca4b240
minseopark0327 <barkmin9@naver.com>
2025-07-05 19:51:29 +0900

    교내근로 scrap 수정2

--- src/main/java/com/example/camjob/dto/WorkPostResponseDto.java
+++ src/main/java/com/example/camjob/dto/WorkPostResponseDto.java
@@ -8,6 +8,7 @@ import java.time.LocalDate;
 @Getter
 @Setter
 public class WorkPostResponseDto {
+    private Long id;
     private String title;
     private String department;
     private String description;
